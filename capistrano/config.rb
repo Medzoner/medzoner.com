848d94ba2cbbd8973b163dccf948b7b9739b7b6a
medzoner <medzoner@medzoner.com>
2017-03-13 12:31:56 +0100

    deploy

--- capistrano/config.rb
+++ capistrano/config.rb
@@ -1,7 +1,7 @@
 set :application, 'medzoner'
 set :repo_url,  "git@bitbucket.org:MEdzoner/medzoner.git"
 
-set :ssh_user, 'root'
+set :ssh_user, 'medzoner'
 server '93.113.206.134', user: fetch(:ssh_user), roles: %w{}
 
 set :scm, :git
