c256fa67c632c876245ad06d1f8ea00183c84503
medzoner <medzoner@medzoner.com>
2017-04-09 19:31:46 +0200

    composer

---
+++ composer.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+
+BASE_PROJECT_UID=0
+
+source "./.env"
+
+CONTAINER_HOME_PATH=/home/www-data
+
+COMPOSE_PATH=$PATHBASE'/docker-compose.yml'
+DOCKER="docker-compose"
+BUILDFLAGS="-f ${COMPOSE_PATH} -p medzoner.com"
+PHP_USER_CMD="/usr/local/bin/gosu www-data"
+
+PROJECT_NAME=
+APP_PATH=
+
+NGINX_HOST=localhost
+NGINX_PATH=
+
+cd ${PATHBASE}
+
+#is ssh agent set ?
+if [ -z "$SSH_AUTH_SOCK" ] ; then
+  eval `ssh-agent -s`
+  ssh-add
+fi
+
+##vendors
+printf "[CMD] %s\n" "${DOCKER} ${BUILDFLAGS} run php ${PHP_USER_CMD} composer install"
+${DOCKER} ${BUILDFLAGS} run php ${PHP_USER_CMD} composer install
