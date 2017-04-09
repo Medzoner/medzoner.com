edb7ca562dae911140a24cf5b2219c1fac1f8a92
medzoner <medzoner@medzoner.com>
2017-04-09 19:00:58 +0200

    diff prod dev

--- docker-files/docker-nginx/symfony-1.11/entrypoint.sh
+++ docker-files/docker-nginx/symfony-1.11/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-#replace vhost hostname by the one provided in env
-sed -i "s/{{NGINX_HOST}}/$NGINX_HOST/g" /etc/nginx/conf.d/default.conf
-
-sed -i "s/{{NGINX_PATH}}/$(echo "$NGINX_PATH" | sed 's_/_\\/_g')/g" /etc/nginx/conf.d/default.conf
-
 exec "$@"
