e69d78efb3bb5b19fd8734426ce853a5a48462d5
medzoner <medzoner@medzoner.com>
2017-04-16 13:40:48 +0200

    deploy local

--- capistrano/config.rb
+++ capistrano/config.rb
@@ -11,7 +11,7 @@ set :log_level, :debug
 
 set :keep_releases, 3
 
-set :linked_files, %w{.env}
+set :linked_files, %w{.env app/config/parameters.yml}
 set :linked_dirs, %w{vendor web/uploads}
 
 
---
+++ capistrano/stages/local.rb
@@ -0,0 +1,5 @@
+set :stage, :local
+set :branch, "master"
+set :rails_env, :production
+
+set :deploy_to,   "/var/www/medzoner"
