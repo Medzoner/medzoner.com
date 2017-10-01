a16c98f032fc74ef7252adb0fb7572357f5a1ea1
Mehdi Youb <medzoner@medzoner.com>
2017-10-01 12:50:09 +0200

    edit nav menu

--- src/Medzoner/GlobalBundle/Menu/Builder.php
+++ src/Medzoner/GlobalBundle/Menu/Builder.php
@@ -22,46 +22,12 @@ class Builder implements ContainerAwareInterface
      */
     public function mainMenu(FactoryInterface $factory, array $options)
     {
-        $request = $this->container->get('request_stack')->getCurrentRequest();
-        $routeName = $request->get('_route');
-
         $menu = $factory->createItem('root');
 
         //Home
         $menu->addChild('HOME', array('route' => 'site'));
-
-        //Blog
-        /**
-          $blogMenu = $menu->addChild('BLOG', array('route' => 'site_blog_homepage'));
-          $aBlogRoutes = array('site_blog_page', 'site_blog_show', 'site_blog_homepage');
-          foreach ($aBlogRoutes as $aBlogRoute) {
-          if ($aBlogRoute == $routeName) {
-          $blogMenu->setCurrent(true);
-          }
-          }
-         */
-
-        //Tools
-        $menuTools = $menu->addChild('+ Plus', array('route' => 'site_pages_proxy', 'attributes' => array('class' => 'has-dropdown not-click')));
-
-        $aToolsRoutes = array('site_book', 'site_pages_proxy', 'site_foundation');
-
-        foreach ($aToolsRoutes as $aToolRoute) {
-            if ($aToolRoute == $routeName) {
-                $menuTools->setCurrent(true);
-            }
-        }
-
-        /**
-        $menuTools->addChild('PROXY', array('route' => 'site_pages_proxy'));
-        $menuTools->addChild('FOUNDATION FRAMEWORK', array('route' => 'site_foundation',));
-         */
-        //$menuTools->addChild('BOOK', array('route' => 'site_book',));
-        $menuTools->addChild('CV', array('route' => 'site_cv',));
-        $menuTools->addChild('CONTACT', array('route' => 'site_contact',));
-
-        $menu->setChildrenAttributes(array('class' => 'right'));
-        $menuTools->setChildrenAttributes(array('class' => 'dropdown'));
+        $menu->addChild('CV', array('route' => 'site_cv',));
+        $menu->addChild('CONTACT', array('route' => 'site_contact',));
 
         return $menu;
     }
