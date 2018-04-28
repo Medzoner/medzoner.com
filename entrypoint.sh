f66839f02c28027eda66ecfe2d470acf6ba2cd52
elmedzoner <elmedzoner@gmail.com>
2018-04-28 16:58:18 +0200

    finalize build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+rm -rf /var/www/app/cache/*
+rm -rf /var/www/app/logs/*
+
+if [ -f /var/www/app/config/parameters.yml ]; then
+    rm -f /var/www/app/config/parameters.yml
+fi
+
 if [ ! -f /var/www/app/config/parameters.yml ]; then
     cp -f /config/parameters.yml /var/www/app/config/parameters.yml
 fi
