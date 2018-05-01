66bbb82ae3bb4a5ded847d29854cb2f8ca1d0ca1
medzoner <medzoner@medzoner.com>
2018-05-01 17:42:06 +0200

    deploy rancher

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 function deploy() {
   echo "deploy:"
-  /usr/bin/rancher --debug up -d --stack "medzoner"
-  /usr/bin/rancher --debug up -d --force-upgrade --pull --stack "medzoner" --confirm-upgrade app-medzoner
+  curl -sL https://github.com/rancher/cli/releases/download/v0.6.1/rancher-linux-amd64-v0.6.1.tar.gz | tar -zx -C . && mv ./rancher-v0.6.1/rancher .
+
+  ./rancher --debug up -d --stack "medzoner"
+  ./rancher --debug up -d --force-upgrade --pull --stack "medzoner" --confirm-upgrade app-medzoner
 }
 
 if [[ $# -ne 1 ]] ; then
