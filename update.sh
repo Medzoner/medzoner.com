814e2a05929875261e0e482adc683967b702802b
medzoner <medzoner@medzoner.com>
2018-09-15 16:04:31 +0200

    new deployer

--- update.sh
+++ update.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-echo ${KUBERNETES_ADMIN_CONF}
-echo ${KUBERNETES_ADMIN_CONF} > admin.conf
-export KUBE_CONFIG=$(pwd)/admin.conf
-
-kubectl get pods
-
 kubectl -n default set image deployment/medzoner-php medzoner-php=registry.medzoner.com/medzoner/medzoner.com
