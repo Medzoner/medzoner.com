9355bf14a7af1c9ddb6faeade7d00ff8e011ac14
medzoner <medzoner@medzoner.com>
2019-10-31 17:12:37 +0100

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,11 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var rev = require('gulp-rev');
 var symlink = require('gulp-symlink');
-var remoteSrc = require('gulp-remote-src');
 
 var dest = './web';
 var src = './assets';
@@ -53,7 +51,7 @@ var versioning = {
     manifestJS: src + '/rev-manifest-js.json'
 };
 
-gulp.task('javascripts', function() {
+gulp.task('javascripts', function(done) {
 
     gulp.src(paths.javascripts)
         .pipe(gulp.dest('assets/vendors/js'))
@@ -63,7 +61,7 @@ gulp.task('javascripts', function() {
         .pipe(gulp.dest('web/js'))
     ;
 
-    return gulp.src(paths.javascripts)
+    gulp.src(paths.javascripts)
         .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(rev())
@@ -72,9 +70,10 @@ gulp.task('javascripts', function() {
         .pipe(gulp.dest(src))
         .pipe(browserSync.stream())
         ;
+    done();
 });
 
-gulp.task('backend-js', function() {
+gulp.task('backend-js', function(done) {
     gulp.src(paths.backendjs)
         .pipe(gulp.dest('assets/vendor/js'))
     ;
@@ -83,7 +82,7 @@ gulp.task('backend-js', function() {
         .pipe(gulp.dest('web/js'))
     ;
 
-    return gulp.src(paths.javascripts)
+    gulp.src(paths.javascripts)
         .pipe(concat('backendjs.min.js'))
         .pipe(uglify())
         .pipe(rev())
@@ -92,13 +91,16 @@ gulp.task('backend-js', function() {
         .pipe(gulp.dest(src))
         .pipe(browserSync.stream())
         ;
+    done();
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function(done) {
     gulp.src(paths.styles)
         .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
         .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('web/css'))
         .pipe(minify())
+        .pipe(gulp.dest('web/css'))
         .pipe(rev())
         .pipe(gulp.dest('web/css'))
         .pipe(rev.manifest(versioning.manifestCss, {base : dest}))
@@ -106,37 +108,42 @@ gulp.task('styles', function() {
         .pipe(browserSync.stream())
     ;
 
-    return gulp.src(paths.styles)
+    gulp.src(paths.styles)
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('app.css'))
         .pipe(gulp.dest('web/css'))
         .pipe(browserSync.stream())
         ;
+    done();
 });
 
-gulp.task('images', function() {
-    return gulp.src(paths.images)
+gulp.task('images', function(done) {
+    gulp.src(paths.images)
     //.pipe(imagemin({optimizationLevel: 5}))
         .pipe(gulp.dest('web/images'))
         .pipe(browserSync.stream());
+    done();
 });
 
-gulp.task('copyfonts', function() {
+gulp.task('copyfonts', function(done) {
     gulp.src(paths.copyfonts)
         .pipe(gulp.dest('./web/fonts'))
         .pipe(browserSync.stream());
     gulp.src(paths.copyfonts)
         .pipe(gulp.dest('./web/css/fonts'))
         .pipe(browserSync.stream());
+
+    done();
 });
 
-gulp.task('symlink', function () {
-    return gulp.src('./assets')
+gulp.task('symlink', function (done) {
+    gulp.src('./assets')
         .pipe(symlink('./web/dev', {force: true}))
     ;
+    done();
 });
 
-gulp.task('watch', ['webserver'], function () {
+gulp.task('watch', function (done) {
 
     for(var key = 0; key < paths.images.length;key++){
         gulp.watch(paths.images, ['images']);
@@ -158,6 +165,7 @@ gulp.task('watch', ['webserver'], function () {
         proxy: "localhost:8182",
         open: false
     });
+    done();
 });
 
 gulp.task('default', ['images', 'javascripts', 'styles', 'copyfonts', 'symlink']);
