45bbcd1793dda5c9a93f85ada524f3a1e28f0d8b
medzoner <medzoner@medzoner.com>
2019-11-04 02:27:45 +0100

    add test and cover

--- tests/Unit/Domain/Model/ModelCollectionTest.php
+++ tests/Unit/Domain/Model/ModelCollectionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Domain\Model;
 
 use ArrayIterator;
+use Medzoner\GlobalBundle\Model\JobBoard\JobBoard;
 use Medzoner\GlobalBundle\Model\ModelCollection;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -11,18 +12,27 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class ModelCollectionTest extends WebTestCase
 {
-    public function testGetNumberOfEmployeeFromLetter()
+    public function test_empty()
     {
         $this->assertEquals(new ModelCollection(), new ModelCollection());
-    }
-
-    public function test_iterator()
-    {
         $this->assertInstanceOf(ArrayIterator::class, (new ModelCollection())->getIterator());
+        $this->assertIsString((new ModelCollection())->__toString());
+        $this->assertFalse((new ModelCollection())->offsetExists(10));
+        $this->assertNull((new ModelCollection())->offsetSet(0, new JobBoard()));
+        $this->assertNull((new ModelCollection())->offsetUnset(10));
     }
 
-    public function test_tostring()
+    public function test_collection()
     {
-        $this->assertIsString((new ModelCollection())->__toString());
+        $jobBoard = (new JobBoard());
+        $collection = new ModelCollection([$jobBoard]);
+        $this->assertCount(1, $collection);
+        $this->assertInstanceOf(ArrayIterator::class, $collection->getIterator());
+        $this->assertIsString($collection->__toString());
+        $this->assertTrue($collection->offsetExists(0));
+        $this->assertNull($collection->offsetSet(1, new JobBoard()));
+        $this->assertCount(2, $collection);
+        $collection->offsetUnset(1);
+        $this->assertCount(1, $collection);
     }
 }
